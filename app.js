60a4dc30f2d32261f04a809e2d5d12b5860408f7
unknown <Raymond-ap>
2021-10-08 11:26:15 -0700

    commits

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const http = require('http')
+
+const placesRoute = require('./routes/places-routes') // Places Router
 
 
 const app = express()
+
+app.use('/api/places',placesRoute)
+
+
+app.listen(5000)
---
+++ routes/places-routes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the famous sky scrapers in the world!",
+    location: {
+      lat: 40.74948770693409,
+      lng: 73.98559187217705,
+    },
+    address: "New York, NY 10001, USA",
+    creator: "u1",
+  },
+];
+
+router.get(`/:pid`, (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => {
+      return p.id === placeId
+  })
+  res.json(place);
+});
+
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId;
+    })
+    res.json(place)
+})
+
+module.exports = router;
