c2b36890578236d1501e713e273dd2c4456c1fac
unknown <Raymond-ap>
2021-10-08 13:16:31 -0700

    commits

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const HttpError = require("../models/http-error");
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -14,6 +16,17 @@ const DUMMY_PLACES = [
     address: "New York, NY 10001, USA",
     creator: "u1",
   },
+  {
+    id: "p2",
+    title: "Midtown South",
+    description: "Dense with skyscrapers, including the Empire State Building,",
+    location: {
+      lat: 40.74797250382559,
+      lng: -73.98227646758923,
+    },
+    address: "New York, NY",
+    creator: "u2",
+  },
 ];
 
 router.get(`/:pid`, (req, res, next) => {
@@ -24,10 +37,12 @@ router.get(`/:pid`, (req, res, next) => {
 
   //Handling Error
   if (!place) {
-    const error = new Error("Could not find a place for the provided id");
-    error.code = 404;
-    throw error
+    // const error = new HttpError("Could not find a place for the provided id", 404);
+    // const error = new Error("Could not find a place for the provided id");
+    // error.code = 404;
+    throw new HttpError("Could not find a place for the provided id", 404);
   }
+  res.json(place);
 });
 
 router.get("/user/:uid", (req, res, next) => {
@@ -38,9 +53,7 @@ router.get("/user/:uid", (req, res, next) => {
 
   //Handling Error
   if (!place) {
-      const error = new Error("Could not find a user for the provided id")
-      error.code = 404
-      next(error)
+    throw new HttpError("Could not find a place for the provided id", 404);
   }
 
   res.json(place);
